9526648de86b280e0c7b4a5645fee53192e03086
SotaShirakawa <souta060614@gmail.com>
2023-05-04 20:15:41 +0900

    phase 2 kansei

--- Soundapp/ViewController.swift
+++ Soundapp/ViewController.swift
@@ -6,14 +6,39 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var drumButton: UIButton!
+    
+    
+    let drumSoundPlayer = try!AVAudioPlayer(data: NSDataAsset(name: "drumSound" )!.data)
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func touchdownDrumButton() {
+        
+        drumButton.setImage(UIImage(named: "drumPlayingImage"), for:  .normal)
+        
+        drumSoundPlayer.currentTime = 0
+        
+        drumSoundPlayer.play()
+    }
+    
+    
+    @IBAction func touchupDrumButton() {
+            
+        drumButton.setImage(UIImage(named: "drumImage"), for: .normal)
+            
+    }
+        
+        
 }
+
+
+
 
